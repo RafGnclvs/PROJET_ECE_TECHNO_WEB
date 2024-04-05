bb8eb7533781d79db6c7e376b91b945cc9d6b305
leticia36 <flsmoussaoui@gmail.com>
2024-04-05 11:40:41 +0200

    resolve player controller

--- back/src/main/java/com/takima/backskeleton/controllers/PlayerController.java
+++ back/src/main/java/com/takima/backskeleton/controllers/PlayerController.java
@@ -2,6 +2,7 @@ package com.takima.backskeleton.controllers;
 
 
 import com.takima.backskeleton.models.Player;
+import com.takima.backskeleton.models.Question;
 import com.takima.backskeleton.models.Student;
 import com.takima.backskeleton.services.PlayerService;
 import com.takima.backskeleton.services.StudentService;
@@ -17,10 +18,8 @@ import java.util.List;
 public class PlayerController {
     private final PlayerService playerService;
 
-
     @GetMapping("")
     public List<Player> getAllPlayers() {
-
         return playerService.findAll();
     }
     @GetMapping("/{id}")
