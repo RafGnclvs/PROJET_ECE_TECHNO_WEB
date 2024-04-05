1b0ed723edbe2fd4a424129f536456ddc7fc1014
Ghitaouan <g.ouannane@gmail.com>
2024-04-05 12:13:49 +0200

    Ajout navbar

--- front/src/app/navbar/navbar.component.ts
+++ front/src/app/navbar/navbar.component.ts
@@ -12,6 +12,7 @@ export class NavbarComponent {
   constructor() {
     this.links.push({ name: "Students", href: "students" })
     this.links.push({ name: "Majors", href: "majors" })
+    this.links.push({ name: "User", href: "player" })
     this.links.push({ name: "Admin", href: "admin" })
 
 
