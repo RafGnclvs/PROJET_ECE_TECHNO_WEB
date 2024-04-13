428e5876af9134f50ac9dc76145b31008d19c2da
Ghitaouan <g.ouannane@gmail.com>
2024-04-14 01:53:10 +0200

    Ajout de la partie Admin(modif question supp question modif reponse) Ghita

--- front/src/app/admin/admin.component.ts
+++ front/src/app/admin/admin.component.ts
@@ -19,16 +19,44 @@ export class AdminComponent implements OnInit {
   response : Response[] = [];
   showPage: number=0;
   currentCategory: number=0;
-  currentQuestion: number=0;
-  currentResponse: number=0;
-  currentAction: string = "";
+  NomsImages : string[]=["Scar_img","Tom_Sawyer_img","Mike_Ross_img"];
+
   path: string=`assets/Question_img/`;
+
+  NewRep : Response =
+    {
+      resp1: "",
+      resp2: "",
+      resp3: "",
+      good_resp : ""
+    }
+  ;
+  NewQuest : Question=
+    {
+      name_img:"",
+      id_cat:0,
+      id_res:0
+    };
+  QuestionSupp : Question=
+    {
+      name_img:"",
+      id_cat:0,
+      id_res:0
+    };
   togglePage(page: number): void {
     this.showPage=page;
   }
 
   constructor(private categoryService : CategoryService, private questionService : QuestionService,private responseService : ResponseService) {
 
+  }
+  Initialisation():void
+  {
+    this.NewRep.resp1="";
+    this.NewRep.resp2="";
+    this.NewRep.resp3="";
+    this.NewRep.good_resp="";
+    this.NewQuest.name_img="";
   }
 
   ngOnInit(): void {
@@ -43,10 +71,60 @@ export class AdminComponent implements OnInit {
     this.currentCategory = idCat;
     this.togglePage(2);
   }
+  ArrierePage():void
+  {
+    this.showPage= this.showPage-1;
+  }
 
   ModifierPage():void
     {
       this.togglePage(1);
     }
+  QuestionPage():void
+  {
+    this.togglePage(3);
+  }
+  AjouterQuestion():void
+  {
+    this.NewQuest.id_cat = this.currentCategory;
+
+
+    this.responseService.addResponse(this.NewRep).subscribe({
+      next: (value) => {
+        this.response.push(value);
+
+
+        console.log(value);
+        this.NewQuest.id_res = Number(value.id_response);
+        this.questionService.addQuestion(this.NewQuest).subscribe({
+          next: (value) => { this.question.push(value);
+            console.log(value);
+            this.Initialisation();
+          },
+        });
+      },
+
+    });
+
+
+
+  }
+
+  SupprimerQuestion(idQuestion : bigint | undefined): void {
+      if (typeof idQuestion !== 'undefined') {
+
+        this.questionService.deleteQuestion(idQuestion).subscribe(() => {
+
+          this.question = this.question.filter(Q => Q.id_question !== idQuestion);
+        });
+      } else {
+        console.error('Tentative de suppression d’un joueur sans ID valide.');
+
+      }
+    }
+
+
   protected readonly Q = Q
 }
+
+
